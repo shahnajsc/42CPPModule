2aeab51af8f4cb64e175d1f8bf64b648c8268498
shahnajsc <sschowdhury29@gmail.com>
2025-10-23 17:29:29 +0300

    main updated for ex00

--- Module_06/ex00/srcs/main.cpp
+++ Module_06/ex00/srcs/main.cpp
@@ -6,10 +6,21 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 15:26:46 by shachowd          #+#    #+#             */
-/*   Updated: 2025/10/20 15:25:31 by shachowd         ###   ########.fr       */
+/*   Updated: 2025/10/23 09:08:43 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ScalarConverter.hpp"
 
+int main(int argc, char **argv)
+{
+	std::string	inputStr;
 
+	if (argc == 1) {
+		std::cout << "No string provided for scalar conversion!" << std::endl;
+		return (1);
+	}
+	inputStr = argv[1];
+	ScalarConverter::convert(inputStr);
+	return (0);
+}
