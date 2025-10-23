16cc77da4f20395b4c7c2e9a96b3b934c4ae7efb
Shahnaj Sharmin Chowdhury <sschowdhury29@.com>
2025-10-23 22:10:16 +0300

    Extra lines removed

--- Module_06/ex00/includes/ScalarConverter.hpp
+++ Module_06/ex00/includes/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 11:32:28 by shachowd          #+#    #+#             */
-/*   Updated: 2025/10/20 17:08:38 by shachowd         ###   ########.fr       */
+/*   Updated: 2025/10/23 21:29:21 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,4 @@ private:
 
 public:
 	static void convert(const std::string &inputStr);
-
 };
