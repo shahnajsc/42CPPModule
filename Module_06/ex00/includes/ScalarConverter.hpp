fe60945f67e9e311037e5117dd7c3d398d8dde9b
shahnajsc <sschowdhury29@gmail.com>
2025-10-24 16:42:36 +0300

    color code added for output

--- Module_06/ex00/includes/ScalarConverter.hpp
+++ Module_06/ex00/includes/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 11:32:28 by shachowd          #+#    #+#             */
-/*   Updated: 2025/10/23 21:29:21 by shachowd         ###   ########.fr       */
+/*   Updated: 2025/10/24 12:13:12 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 #include <iostream>
 #include <cmath>
 
+#define GREEN		"\033[0;92m"
+#define BLUE		"\033[0;94m"
+#define RED			"\033[0;31m"
+#define YELLOW		"\033[93m"
+#define PINK		"\033[0;35m"
+#define RESET		"\033[0m"
 
 enum Type {
 	CHAR,
