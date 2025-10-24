229f56c3ded0014f8efc86977772776167f298ac
shahnajsc <sschowdhury29@gmail.com>
2025-10-24 16:42:58 +0300

    color code added for output

--- Module_06/ex02/includes/Base.hpp
+++ Module_06/ex02/includes/Base.hpp
@@ -6,11 +6,17 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 11:32:28 by shachowd          #+#    #+#             */
-/*   Updated: 2025/10/23 20:45:57 by shachowd         ###   ########.fr       */
+/*   Updated: 2025/10/24 12:01:13 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#define GREEN		"\033[0;92m"
+#define RED			"\033[0;31m"
+#define YELLOW		"\033[93m"
+#define PINK		"\033[0;35m"
+#define RESET		"\033[0m"
 
 #include <iostream>
 
