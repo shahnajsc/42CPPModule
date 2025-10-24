2a129c51aee5aab60b738bfa1ab242ccd2a3efb5
shahnajsc <sschowdhury29@gmail.com>
2025-10-24 16:43:59 +0300

    logic updated for argc

--- Module_06/ex00/srcs/main.cpp
+++ Module_06/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 15:26:46 by shachowd          #+#    #+#             */
-/*   Updated: 2025/10/23 09:08:43 by shachowd         ###   ########.fr       */
+/*   Updated: 2025/10/24 12:13:44 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int main(int argc, char **argv)
 {
-	std::string	inputStr;
 
-	if (argc == 1) {
-		std::cout << "No string provided for scalar conversion!" << std::endl;
+	if (argc != 2) {
+		std::cout << RED << "Wrong parameter(s) provided for conversion!" << RESET << std::endl;
 		return (1);
 	}
-	inputStr = argv[1];
+
+	std::string	inputStr = argv[1];
 	ScalarConverter::convert(inputStr);
 	return (0);
 }
