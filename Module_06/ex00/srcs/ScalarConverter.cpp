88de2d20dee2e4872d45d54060db049a0d682929
shahnajsc <sschowdhury29@gmail.com>
2025-10-24 16:44:35 +0300

    Type identification done for float and double

--- Module_06/ex00/srcs/ScalarConverter.cpp
+++ Module_06/ex00/srcs/ScalarConverter.cpp
@@ -6,11 +6,12 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 12:00:32 by shachowd          #+#    #+#             */
-/*   Updated: 2025/10/23 22:07:09 by shachowd         ###   ########.fr       */
+/*   Updated: 2025/10/24 15:34:45 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ScalarConverter.hpp"
+#include <iomanip>
 
 bool isPseudoLiterals(const std::string &inputStr) {
 
@@ -28,39 +29,117 @@ bool isPseudoLiterals(const std::string &inputStr) {
 
 bool isInt(const std::string &inputStr) {
 
-}
+	size_t dot = inputStr.find('.');
 
-bool isFloat(const std::string &inputStr) {
+	if (dot == inputStr.npos) {
+
+		size_t i = 0;
+		if (inputStr[i] == '+' || inputStr[i] == '-')
+			i++;
+		for (; i < inputStr.length(); i++) {
+			if (!isdigit(inputStr[i]))
+				return false;
+		}
+		return true;
+	}
+	return false;
 
 }
 
 bool isDouble(const std::string &inputStr) {
 
+	size_t len = inputStr.length();
+
+	size_t dot = inputStr.find('.');
+
+	std::cout << "DOT: " << dot << std::endl;
+
+	if (dot != std::string::npos) {
+
+		size_t i = 0;
+		if ((inputStr[i] == '+' || inputStr[i] == '-') && len > 2)
+			i++;
+		for (; i < len; i++) {
+			if (!isdigit(inputStr[i]) && inputStr[i] != '.')
+				return false;
+		}
+		return true;
+	}
+	return false;
+}
+
+bool isFloat(const std::string &inputStr) {
+
+	size_t len = inputStr.length();
+
+	if (len < 2)
+		return false;
+
+	if (isDouble(inputStr.substr(0, len -1)) && inputStr[len - 1] == 'f')
+		return true;
+	return false;
 }
 
 void printChar(const std::string &inputStr) {
 	char input = inputStr[0];
 
-	std::cout << "Input is CHAR" << std::endl;
+	std::cout << std::endl << GREEN << "Input is CHAR" << RESET << std::endl;
 
-	std::cout << "CHAR: " << input << std::endl;
-	std::cout << "INT: " << static_cast<int>(input) << std::endl;
-	std::cout << "FLOAT: " << static_cast<float>(input) << std::endl;
-	std::cout << "DOUBLE: " << static_cast<double>(input) << std::endl;
+	std::cout << "  CHAR:   " << input << std::endl;
+	std::cout << "  INT:    " << static_cast<int>(input) << std::endl;
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << "  FLOAT:  " << static_cast<float>(input) << "f" << std::endl;
+	std::cout << "  DOUBLE: " << static_cast<double>(input) << std::endl << std::endl;
+}
+
+void printInt(const std::string &inputStr) {
+	int input = stoi(inputStr);
+
+	std::cout << std::endl << GREEN << "Input is INT" << RESET << std::endl;
+
+	std::cout << "  CHAR:   " << static_cast<char>(input) << std::endl;
+	std::cout << "  INT:    " << input << std::endl;
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << "  FLOAT:  " << static_cast<float>(input) << "f" << std::endl;
+	std::cout << "  DOUBLE: " << static_cast<double>(input) << std::endl << std::endl;
+}
+
+void printDouble(const std::string &inputStr) {
+	int input = stod(inputStr);
+
+	std::cout << std::endl << GREEN << "Input is DOUBLE" << RESET << std::endl;
+
+	std::cout << "  CHAR:   " << static_cast<char>(input) << std::endl;
+	std::cout << "  INT:    " << static_cast<int>(input) << std::endl;
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << "  FLOAT:  " << static_cast<float>(input) << "f" << std::endl;
+	std::cout << "  DOUBLE: " << input << std::endl << std::endl;
+}
+
+void printFloat(const std::string &inputStr) {
+	int input = stof(inputStr);
+
+	std::cout << std::endl << GREEN << "Input is FLOAT" << RESET << std::endl;
+
+	std::cout << "  CHAR:   " << static_cast<char>(input) << std::endl;
+	std::cout << "  INT:    " << static_cast<int>(input) << std::endl;
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << "  FLOAT:  " << input << std::endl;
+	std::cout << "  DOUBLE: " <<  static_cast<double>(input) << std::endl << std::endl;
 }
 
 Type getType(const std::string &inputStr) {
 
-	if (inputStr.length() == 1 && !isdigit(inputStr[0]))
+	if (inputStr.length() == 1 && !isdigit(inputStr[0]) && isprint(inputStr[0]))
 		return CHAR;
 	else if (isPseudoLiterals(inputStr))
 		return PSEUDO;
 	else if (isInt(inputStr))
 		return INT;
-	else if (isFloat(inputStr))
-		return FLOAT;
 	else if(isDouble(inputStr))
 		return DOUBLE;
+	else if (isFloat(inputStr))
+		return FLOAT;
 	else
 		return INVALID;
 }
@@ -72,9 +151,19 @@ void ScalarConverter::convert(const std::string &inputStr) {
 	if (type == CHAR) {
 		printChar(inputStr);
 	}
-	else if (type == INT) {
-		
+	else if (type == PSEUDO) {
+		std::cout << "PSEUDO it is" << std::endl;
 	}
-
-
+	else if (type == INT) {
+		printInt(inputStr);
+	}
+	else if (type == DOUBLE) {
+		printDouble(inputStr);
+	}
+	else if (type == FLOAT) {
+		printFloat(inputStr);
+	}
+	else if (type == INVALID) {
+		std::cout << "INVALID INPUT" << std::endl;
+	}
 }
