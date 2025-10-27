072b8601cc8cee1ddc2a7dbf6b2715d8229a242e
Shahnaj Sharmin Chowdhury <sschowdhury29@.com>
2025-10-27 16:33:09 +0200

    m06ex00: final version

--- Module_06/ex00/srcs/main.cpp
+++ Module_06/ex00/srcs/main.cpp
@@ -6,11 +6,12 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 15:26:46 by shachowd          #+#    #+#             */
-/*   Updated: 2025/10/24 12:13:44 by shachowd         ###   ########.fr       */
+/*   Updated: 2025/10/27 11:06:03 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ScalarConverter.hpp"
+#include <iostream>
 
 int main(int argc, char **argv)
 {
