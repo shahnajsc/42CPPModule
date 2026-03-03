ba02855d1b7cb4a70d67a828123c9f4d42568cff
shahnajsc <sschowdhury29@gmail.com>
2026-03-03 22:06:33 +0200

    m07ex02: Added required class

--- Module_07/ex02/includes/Array.hpp
+++ Module_07/ex02/includes/Array.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 #define GREEN		"\033[0;92m"
 #define RED			"\033[0;31m"
@@ -8,15 +9,21 @@ template<typename T>
 class Array
 {
 private:
-	size_t	arrSize;
-	T		*arr;
+	unsigned int	arrSize;
+	T				*arr;
 
 public:
 	Array();
 	Array(unsigned int n);
 
 	Array(const Array &copyCons);
-	Array& operator= (const Array& otherCons);
+	Array& operator= (const Array &otherCons);
+
+	T& operator[](unsigned int _arrIndex);
+	const T& operator[](unsigned int _arrIndex) const;
+	unsigned int size() const;
 
 	~Array();
 };
+
+
